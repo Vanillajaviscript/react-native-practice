9bc4a828a526c93b88796fc2552a8caac58295e9
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-07-29 14:15:11 -0700

    adds user model

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const logger = require('morgan')
+const connectDB = require('./config/connection')
 const PORT = process.env.PORT || 8000
-
+const colors = require('colors');
 
 
 app.get('/', (req, res) => {
   res.send('<h1 style="color: red;"> root directory</h1>')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`)
-})
+/////////////
+//Server listener
+/////////////
+const start = async () => {
+  try {
+    connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is live on port: ${PORT}`)
+    })
+  } catch (error) {
+    console.log(`Catch error: ${error}`)
+  }
+};
+
+start();
