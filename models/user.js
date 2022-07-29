6fccfc085b134832f058b87bcbd4108c370bea23
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-07-29 14:28:49 -0700

    adds successful user creation to database

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,4 +25,29 @@ const userSchema = new mongoose.Schema(
   }
 )
 
-model.exports = mongoose.model('User', userSchema)
+userSchema.set("toJSON", {
+  transform: function (doc, ret) {
+    // remove the password property when serializing doc to JSON
+    delete ret.password;
+    return ret;
+  },
+});
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.comparePassword = function(tryPassword, cb) { // cant use arrow function here
+  bcrypt.compare(tryPassword, this.password, cb);
+};
+const User = mongoose.model("User", userSchema);
+
+module.exports = User
--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const logger = require('morgan')
 const connectDB = require('./config/connection')
 const PORT = process.env.PORT || 8000
 const colors = require('colors');
+const User = require('./models/user')
+const cors = require('cors')
+/////////////
+//Middleware
+/////////////
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/create-user', async (req, res) => {
+  const user = await User({fullName: 'Javier Delgado', email: 'vanillajaviscript@gmail.com', password: 'Password12345'});
+  await user.save();
+  res.json(user);
+})
 
 app.get('/', (req, res) => {
   res.send('<h1 style="color: red;"> root directory</h1>')
